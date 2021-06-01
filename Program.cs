24b17a44e6334a758486ab405acb54f41977eb33
Stefan Negulescu <tarthedark@gmail.com>
2021-06-01 19:28:47 +0300

    Use minimal APIs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-﻿using diabloblazor.Server;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Hosting;
 
-BuildWebHost(args).Run();
+var app = WebApplication.CreateBuilder(args).Build();
 
-static IWebHost BuildWebHost(string[] args) =>
-    WebHost.CreateDefaultBuilder(args)
-        .UseConfiguration(new ConfigurationBuilder()
-            .AddCommandLine(args)
-            .Build())
-        .UseStartup<Startup>()
-        .Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".mpq"] = "application/octet-stream";
+app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
+
+app.UseBlazorFrameworkFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints => endpoints.MapFallbackToFile("index.html"));
+
+app.Run();
