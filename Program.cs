7fef48aa5068c008d2222b20b660363785118b90
Stefan Negulescu <tarthedark@gmail.com>
2021-06-09 17:20:51 +0300

    Add way to serve published (WebAssembly AOT compiled) app

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Hosting;
 
-var app = WebApplication.CreateBuilder(args).Build();
+var builder = WebApplication.CreateBuilder(args);
+
+// Serve published (WebAssembly AOT compiled) app
+//builder.WebHost.UseWebRoot(@$"{builder.Environment.WebRootPath}\..\diabloblazor\bin\Release\net6.0\publish\wwwroot");
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
