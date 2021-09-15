f0daefee389812fdab4ffead775bf4685bc46cd6
Stefan Negulescu <tarthedark@gmail.com>
2021-09-15 20:00:51 +0300

    Upgrade to .NET 6 RC1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.StaticFiles;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Hosting;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
